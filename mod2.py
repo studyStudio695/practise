f835cd576750887d996c6c5346aa890cc249a9f7
studyStudio695 <51707143+studyStudio695@users.noreply.github.com>
2019-06-12 15:39:06 +0530

    Update mod2.py

--- mod2.py
+++ mod2.py
@@ -1,1 +1,19 @@
+def CheckAcc(YN):
+  if YN == 'Y':
+    value = True
+  else:
+    value = False
+  return value
+    
+class user:
+    __id = 0
+    __passcode:none
 
+
+    def __init__(self, id, passcode):
+        self.__id = id
+        self.__passcode= passcode
+  
+def login(id,passcode):
+  id = user(id,passcode)
+  
