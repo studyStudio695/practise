470e1dff2aab438fe6a3835b51e9f374f55b5a2b
studyStudio695 <51707143+studyStudio695@users.noreply.github.com>
2019-06-12 13:02:10 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,23 @@
-if true:
-  print("hello")
-else :
-  pass
+# Main
+"""
+  author: STUDY STUDIO
+  
+"""
+import mod1 as m1
+import os
+import mod2 as m2
+
+m1.speak('welcome to our program')
+m1.speak('Do You Have An Account')
+AccYN = input('Answer In Y or N >> ')
+if m2.CheckAcc(AccYN):
+    m1.speak('So Enter Your Id')
+    Id = input("ID >> ")
+    m1.speak("And Enter Your pass")
+    passcode = input("PASS >>")
+    m2.login(Id,password)
+elif m2.CheckAcc(AccYN) == False:
+    input()
+  
+  
+  
